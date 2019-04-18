7c9e390135e6247a5407c3ec817c6f45e0b2f88d
github username <github email address>
2019-04-18 12:12:45 -0400

    switching to group review branch

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 // It might be a good idea to add event listener to make sure this file
 // only runs after the DOM has finshed loading.
 
-// When <some event> happens, I want to make a <what kind of> fetch and then manipulate the DOM <in what way?>
+// When DOM loads happens, I want to make a GET fetch and then manipulate the DOM by rendering the quotes
