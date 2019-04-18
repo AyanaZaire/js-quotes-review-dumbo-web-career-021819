eed0dae76498d9786e899800c356906df1862786
AyanaZaire <ayanazaire@gmail.com>
2019-04-18 09:44:59 -0400

    start fresh

--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
-// It might be a good idea to add event listener to make sure this file 
+// It might be a good idea to add event listener to make sure this file
 // only runs after the DOM has finshed loading.
+
+// When <some event> happens, I want to make a <what kind of> fetch and then manipulate the DOM <in what way?>
